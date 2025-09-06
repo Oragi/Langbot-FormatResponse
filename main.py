70fc95368ef88a2c5cf66f45e3c1ab21686a0290
Oragi <13247578+Oragi@users.noreply.github.com>
2025-09-06 13:49:32 +0000

    initial edit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pkg.plugin.events import *  # 导入事件类
 
 # 注册插件
 @register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(BasePlugin):
+class FormatResponsePlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
@@ -15,34 +15,24 @@ class HelloPlugin(BasePlugin):
         pass
 
     # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+    @handler(NormalMessageResponded)
+    async def normal_message_responded(self, ctx: EventContext):
+        msg = ctx.event.response_text  # 这里的 event 即为 PersonNormalMessageReceived 的对象
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+        prefix_pattern = (
+            r"^\s*⊰•-•✧•-•-⦑/L\\O\\V\\E/\\P\\L\\I\\N\\Y/⦒-•-•✧•-•⊱\s*"
+            r"(sure i can here you are fren:?|Sure I can! Here you are fren:?)?\s*"
+        )
+        response_text = re.sub(prefix_pattern, "", response_text, flags=re.IGNORECASE | re.MULTILINE)
+        # Trim leading and trailing whitespace
+        response_text = response_text.strip()
+        event["response_text"] = response_text
 
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+        # 回复消息 "hello, <发送者id>!"
+        ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
 
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+        # 阻止该事件默认行为（向接口获取回复）
+        ctx.prevent_default()
 
     # 插件卸载时触发
     def __del__(self):
